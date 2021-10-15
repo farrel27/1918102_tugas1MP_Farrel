1c65211dc87574f57d098c93bef01bb88ad3c47c
farrel27 <farrelart99@gmail.com>
2021-10-15 20:24:26 +0700

    first commit

---
+++ app/src/main/java/com/example/tugas1mp_profil1918102/layout_profil1918102.java
@@ -0,0 +1,16 @@
+package com.example.tugas1mp_profil1918102;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class layout_profil1918102 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_profil1918102);
+    }
+
+
+}
